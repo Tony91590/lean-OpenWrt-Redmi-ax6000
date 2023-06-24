29cb480397996bed78421cee8bbebf19747f8a91
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-06-24 13:28:44 +0200

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,8 @@
 
 # Custom for REDMI AX6000
 sed -i 's/zh_cn/auto/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
+sed -i 's/zh_cn/auto/g' feeds/luci/ applications/luci-app-vlmcsd/root/etc/uci-defaults/luci-vlmcsd/luci-vlmcsd
+
 #======================
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/ddns.config feeds/packages/net/ddns-scripts/files/
 #======================
