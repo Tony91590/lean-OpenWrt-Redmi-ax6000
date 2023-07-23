d2707fef07ac43ec756762d0f042368f8c2631a5
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-07-23 16:07:44 +0200

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,7 +27,6 @@ cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/default-settings/Makefile package/le
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/autocore/index.htm package/lean/autocore/files/arm/
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/zones.lua feeds/luci/applications/luci-app-firewall/luasrc/model/cbi/firewall/
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/lib/mac80211.sh package/kernel/mac80211/files/lib/wifi/
-cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/lib/mac80211.sh
-package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/netifd/mac80211.sh package/kernel/mac80211/files/lib/netifd/wireless/
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
