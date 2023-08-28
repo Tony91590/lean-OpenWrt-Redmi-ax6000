638d1de01cbf8f33785393ee84d94be8d49edea8
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-08-28 23:16:29 +0200

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Custom for REDMI AX6000
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's,system.ntp.enable_server='1',system.ntp.enable_server='0',g' package/base-files/files/bin/config_generate
 sed -i 's/ntp.aliyun.com/0.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 sed -i 's/time1.cloud.tencent.com/1.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
 sed -i 's/time.ustc.edu.cn/2.openwrt.pool.ntp.org/g' package/base-files/files/bin/config_generate
